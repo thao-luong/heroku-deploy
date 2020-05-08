0b1cc40cf76172dcdbc5c103863b889aa306c5b6
Thao-Luong <thao.luong@gooddata.com>
2020-05-08 17:00:49 +0700

    initial heroku app

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/ui-sdk-angular-examples'));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.get('/*', function(req,res) {
 
 res.sendFile(path.join(__dirname +'/dist/ui-sdk-angular-examples/index.html'));
