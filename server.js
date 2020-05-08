296f1f30e3cfca14a86477686d695736662f4cc8
Thao-Luong <thao.luong@gooddata.com>
2020-05-08 09:41:43 +0700

    initial heroku app

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 //Install express server
-// const express = require('express');
-// const path = require('path');
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
 // // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
-// app.get('/*', function(req,res) {
+app.get('/*', function(req,res) {
     
-// res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-// });
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
 
 // // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080);
-
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
-
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
-
-var port = process.env.PORT || 8000
-app.listen(port)
-console.log('server started ' + port)
+app.listen(process.env.PORT || 8080);
